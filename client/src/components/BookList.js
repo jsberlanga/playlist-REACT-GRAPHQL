b8ac73d196570020694a29c126a0dfa9e40bfe14
jsberlanga <v-jusoto@microsoft.com>
2019-03-16 01:53:53 +0100

    book details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  state = {
+    selected: null
+  };
   displayBooks = () => {
     const data = this.props.data;
     if (data.loading) return <p>Loading books...</p>;
     else {
       return data.books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li key={book.id} onClick={e => this.setState({ selected: book.id })}>
+            {book.name}
+          </li>
+        );
       });
     }
   };
   render() {
+    const { selected } = this.state;
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={selected} />
       </div>
     );
   }
